ac93c690f47ab8fba69a8cbdf4eddc3947d312e3
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-06-02 15:18:36 -0300

    c6-just to added my reports into my appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,16 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import {User} from "src/users/users.entity"
-
+import { Reports } from './reports/reports.entity';
 
 
 @Module({
   imports: [TypeOrmModule.forRoot({ 
     type:"sqlite",
     database:"db.sqlite",
-    entities:[User],
+    entities:[User, Reports],
     synchronize: true,
-  }) ,  UsersModule, ReportsModule],
+  }) ,  UsersModule,  ReportsModule],
   
   controllers: [AppController],
   providers: [AppService],
