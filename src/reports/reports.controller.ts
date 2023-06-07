d00894fd5b9ca6c884cc45191d30833fc467fccf
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-06-06 21:34:46 -0300

    to starting create and put my hands in reports fol

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('reports')
-export class ReportsController {}
+@Controller("reports")
+export class ReportsController {
+ 
+  @Get("test")
+  createJust() {
+    console.log("testing my reports here what i have to change")
+  }
+
+}
