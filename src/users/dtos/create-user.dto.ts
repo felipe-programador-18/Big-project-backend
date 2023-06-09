aa5766dd8fb8d4ff06a09d3ce3f0b568493630de
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-06-09 15:03:46 -0300

    just testing in put my hands in class validator

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,4 +1,4 @@
-import  {IsString, IsEmail, IsStrongPassword} from 'class-validator'
+import  {IsString, IsEmail, IsStrongPassword, isStrongPassword} from 'class-validator'
 
 
 export class CreateUserDto {
