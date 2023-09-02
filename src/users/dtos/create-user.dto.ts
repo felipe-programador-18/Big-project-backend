887662c09979c5445eb4f92537c55f9a8ef46893
Felipe <machadofelipe2016@outlook.com>
2023-09-02 15:51:18 -0300

    "come back to practice biggiest backend projet"

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,6 +1,8 @@
 import  {IsString, IsEmail, IsStrongPassword, isStrongPassword} from 'class-validator'
 
 
+// just small structure to come back put my hands on code!!!
+
 export class CreateUserDto {
   @IsEmail()
   email: string;
