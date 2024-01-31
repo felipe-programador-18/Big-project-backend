b0bf6db52f4f35e6b4d0a337efe452ff22e8242a
Felipe <machadofelipe2016@outlook.com>
2024-01-31 11:05:29 -0300

    reorganized my code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import {User} from "src/users/users.entity"
-import { Reports } from './reports/reports.entity';
-import { UsersController } from './users/users.controller';
-import { ReportsController } from './reports/reports.controller';
-import { UsersService } from './users/users.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { ReportsModule } from "./reports/reports.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "src/users/users.entity";
+import { Reports } from "./reports/reports.entity";
+import { UsersController } from "./users/users.controller";
+import { ReportsController } from "./reports/reports.controller";
+import { UsersService } from "./users/users.service";
 
 @Module({
-  imports: [TypeOrmModule.forRoot({ 
-    type:"sqlite",
-    database:"db.sqlite",
-    entities:[User, Reports],
-    synchronize: true,
-  }),UsersModule,  ReportsModule],
-  
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "db.sqlite",
+      entities: [User, Reports],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, Get, Query, Delete, Patch, Param } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UsersService } from "./users.service";
+import { UpdateUserDto } from "src/users/dtos/update-user.dto";
 
 @Controller("auth")
 export class UsersController {
@@ -24,5 +25,11 @@ export class UsersController {
   @Delete("/:id")
   removeUser(@Param("id") id: string) {
     return this.userService.remove(parseInt(id));
+  }
+
+  // this is my update
+  @Patch("/:id")
+  updateUser(@Param("id") id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
   }
 }
