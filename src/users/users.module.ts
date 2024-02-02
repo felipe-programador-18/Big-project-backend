0b1dff6aa3a70058958f310ed202237a1a0876e5
Felipe <machadofelipe2016@outlook.com>
2024-02-02 15:28:14 -0300

    just to rearrange something in my code

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users.entity';
-import { CreateUserDto } from './dtos/create-user.dto';
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./users.entity";
+import { CreateUserDto } from "./dtos/create-user.dto";
 
 @Module({
-  imports:[TypeOrmModule.forFeature([User])] ,
-  
+  imports: [TypeOrmModule.forFeature([User])],
+
   controllers: [UsersController],
-  providers: [UsersService, CreateUserDto]
+  providers: [UsersService, CreateUserDto],
 })
 export class UsersModule {}
