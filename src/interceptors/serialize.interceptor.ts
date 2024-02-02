d7500d6e3c3c38182bf65aa3313d9eb58bccb30a
Felipe <machadofelipe2016@outlook.com>
2024-02-02 17:24:31 -0300

    to rearrange my code and to starting putting together my  interceptions to manage my date

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,6 @@
+import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { plainToClass } from "class-transformer";
+
+// putting together glabally structure to manage my data
--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,6 +1,8 @@
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 import { Exclude } from "class-transformer";
 
+// I will put together other way to interceptions my password and this stuff like that
+
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
