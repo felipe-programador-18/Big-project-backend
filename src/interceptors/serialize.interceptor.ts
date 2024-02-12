a1c27b702ba9448f64181e7cc78263c42ba01f16
Felipe <machadofelipe2016@outlook.com>
2024-02-12 14:00:35 -0300

    come back buliding my largerst backend project

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -4,3 +4,18 @@ import { map } from "rxjs/operators";
 import { plainToClass } from "class-transformer";
 
 // putting together glabally structure to manage my data
+export class SerialiazeInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, handler: CallHandler<any>): Observable<any> {
+    //  run something before a resquest is handled
+    // by the request handler
+
+    console.log("I runnning before the handler", context);
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        // Im running before the sent out request
+        console.log("running before sent out", data);
+      })
+    );
+  }
+}
